a7f771b9024f31076c7095b3a536d6cb615ea0b6
Lucas Melo de Carvalho <lucas_m@id.uff.br>
2023-06-27 10:08:13 -0300

    funcionalidade 1 ta pronta pros cria

--- testes.cpp
+++ testes.cpp
@@ -9,14 +9,14 @@ using namespace std;
 
 - Primeira funcionalidade: processar CSVs em lote
 i)  O arquivo não existir ou não puder ser aberto (x)
-ii) Uma criança aparecer mais de uma vez (em linhas diferentes) no mesmo arquivo
+ii) Uma criança aparecer mais de uma vez (em linhas diferentes) no mesmo arquivo (x)
 iii) A quantidade de vezes que uma criança executou um quesito não existir ou não
-     for um número inteiro positivo
+     for um número inteiro positivo  ( )
 
-- Segunda funcionalidade: armazenar as estruturas vector<registro> e vector<ocorrencia> num arquivo binário (.dat)
+- Segunda funcionalidade: armazenar as estruturas vector<registro> e vector<ocorrencia> num arquivo binário (.dat) ( )
 - Terceira funcionalidade: recuperar as informações salvas em um arquivo binário
   as estruturas existentes naquele momento deve ser limpas/esvaziadas
-  e devem ser construídas a partir das informações vindas do arquivo binário.
+  e devem ser construídas a partir das informações vindas do arquivo binário. ( )
 
 - Opcional: modularizar o programa (fazer os .h)
 
@@ -36,15 +36,21 @@ int busca(string nome, vector<vector<string>> &matriz)
   return 0;
 }
 
-int readfile2 (string nomeArquivo, vector<vector<string>> &matriz, string &crianca)
+bool isNumeric(string const &str)
 {
+  char *p;
+  strtol(str.c_str(), &p, 10);
+  return *p == 0;
+}
 
-  string line, word;
-  vector<string> row;
-  fstream file;
-  file.open(nomeArquivo, ios::in);
-  if (!file)
-  {
+int readfile2(string nomeArquivo, vector<vector<string>> &matriz, string &crianca, string &ques)
+{
+    string line, word;
+    vector<string> row;
+    fstream file;
+    file.open(nomeArquivo, ios::in);
+    if (!file)
+    {
     cout << "Arquivo " << nomeArquivo << " nao existe" << endl;
     return 0;
   }
@@ -59,8 +65,24 @@ int readfile2 (string nomeArquivo, vector<vector<string>> &matriz, string &crian
       if (repete == 0)
       {
         row.push_back(word);
+        crianca = word;
         while (getline(str, word, ','))
         {
+          string quesito = word;
+          getline(str, word, ',');
+          if (!isNumeric(word))
+          {
+            ques = quesito;
+            matriz.push_back(row);
+            return 3;
+          }
+          if (stoi(word) <= 0)
+          {
+            ques = quesito;
+            matriz.push_back(row);
+            return 3;
+          }
+          row.push_back(quesito);
           row.push_back(word);
         }
         matriz.push_back(row);
@@ -90,20 +112,20 @@ void show(vector<vector<string>> matriz)
 
 void processamentoLote(string arq, vector<vector<string>> &m)
 {
-  string name = arq + ".txt", linha;
+  string entrada = arq + ".txt", linha;
   fstream aux;
-  aux.open(name, ios::in);
+  aux.open(entrada, ios::in);
   if (!aux)
   {
-    cout << "O arquivo " << name << " nao existe." << endl;
+    cout << "O arquivo " << entrada << " nao existe." << endl;
   }
   else
   {
     while (getline(aux, linha))
     {
       linha += ".csv";
-      string kid;
-      int check = readfile2(linha, m, kid);
+      string nome, quesito;
+      int check = readfile2(linha, m, nome, quesito);
       if (check == 0)
       {
         fstream log;
@@ -112,6 +134,7 @@ void processamentoLote(string arq, vector<vector<string>> &m)
         log << "Linha: 0" << endl;
         log << "O arquivo nao existe ou nao pode ser aberto" << endl
             << endl;
+        log.close();
       }
       if (check == 2)
       {
@@ -119,13 +142,26 @@ void processamentoLote(string arq, vector<vector<string>> &m)
         log.open("log.txt", ios::app);
         log << "Arquivo: " << linha << endl;
         log << "Linha: " << m.size() + 1 << endl;
-        log << "A crianca " << kid << " se repete em linhas diferentes no arquivo" << endl
+        log << "A crianca " << nome << " se repete em linhas diferentes no arquivo" << endl
             << endl;
+        log.close();
+      }
+      if (check == 3)
+      {
+        fstream log;
+        log.open("log.txt", ios::app);
+        log << "Arquivo: " << linha << endl;
+        log << "Linha: " << m.size() << endl;
+        log << "A quantidade que " << quesito << " possui em " << nome
+            << " nao existe ou nao e um inteiro positivo" << endl
+            << endl;
+        log.close();
       }
       show(m);
       cout << endl;
       m.clear();
     }
+    aux.close();
   }
 }
 
